53bf035f44c510bb4aecef64bff40f6061ca2b8e
Sarah Raguin <sarahraguin@gmail.com>
2022-04-26 14:15:17 -0400

    Last changes and last commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+This application is a hot sauce's galery that you can like, dislike and even post yours to share your taste with others.
+
+Servers:
+
+To begin, npm install.
+To start frontend server : in terminal, go to frontend/Web-Developer-P6 with the instruction ng serve
+To start backend server : in terminal, go to backend/Raguin_Sarah_1_CodeApi_032022 with the insturction npm start
--- app.js
+++ app.js
@@ -46,9 +46,9 @@ app.use(express.json());
 
 // gestion des CORS : Cross Origin Resource Sharing : bloque appels http entre serveurs différents
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
 });
 app.use(cors({
